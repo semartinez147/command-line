14b6f69b845882d146c27a4d8f7065f9be997b9f
Samuel Martinez <semartinez147@gmail.com>
2020-01-13 15:09:49 -0700

    Reading scanner for input.  Not doing math yet.

--- src/edu/cnm/deepdive/CommandLine.java
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -1,25 +1,48 @@
 package edu.cnm.deepdive;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class CommandLine {
 
   public static void main(String[] args) {
-    int[] vals = parse(args); //fill array "vals" from the "parse" method;
-    int total = sum(vals); //add the items in "vals" from the "sum" method;
+    Double[] data = read();
+/*
+    int total = sum(vals); //define the integer "total" by running the "sum" method on the array "vals";
     System.out.println(total);
+*/
   }
 
   private static int[] parse(String[] args) { //the method is called "parse";
     int[] values = new int[args.length]; //declare array of ints named "values," make it as big as the number of arguments from the command line;
     for (int i = 0; i < args.length; i++) {
-      values[i] = Integer.parseInt(args[i]); //turn arguments into integers and add them to the array "values";
+      values[i] = Integer.parseInt(args[i]); //turn arguments from command line into integers and add them to the array "values";
     }
     return values;
   }
+
   private static int sum(int[] data) { //the method is called "sum";
     int total = 0;
     for (int i = 0; i < data.length; i++) {
-      total += data[i]; //add element i (which increments) of the array data to total;
+      total += data[i]; //add the integer in position i (which increments) of the array data to total;
     }
     return total;
+  }
+
+  private static Double[] read() {
+    Double[] data = null;
+    Scanner scanner = new Scanner(System.in); //puts a Scanner in the stack, named "scanner," that refers to a Scanner object in the heap;
+    List<Double> input = new LinkedList<>();
+    while (true) {
+      try {
+        input.add(scanner.nextDouble());
+      } catch (NoSuchElementException e) {
+        break;
+      }
+    }
+    data = input.toArray(new Double[0]);//the program will size the array itself
+    return data;
   }
 }
